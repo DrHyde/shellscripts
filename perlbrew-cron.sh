eaecde9fdf11dd9758c517417bcf28156ceba6b7
David Cantrell <david@cantrell.org.uk>
2019-01-06 16:33:15 +0000

    script to ensure environment is correct when running perl stuff from cron

---
+++ perlbrew-cron.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+#
+# This software is copyright (c) 2013 by Andy Gorman.
+#
+# This is free software; you can redistribute it and/or modify it under the same
+# terms as the Perl 5 programming language system itself.
+#
+# Copied from https://github.com/agorman/perlbrew-cron
+
+COMMAND=${1:+"$@"}
+
+PERLBREW_HOME=${PERLBREW_HOME:=$HOME/.perlbrew}
+
+function error() {
+    echo $1
+    exit 1;
+}
+
+if [ -z "$COMMAND" ]; then
+    error "Usage: perlbrew-cron <command>"
+fi
+
+if [ ! -f "$PERLBREW_HOME/init" ]; then
+    error "no such file: $PERLBREW_HOME/init";
+fi
+
+source $"$PERLBREW_HOME/init"
+
+if [ -z $PERLBREW_ROOT ]; then
+    error "unable to load PERLBREW_ROOT environment variable"
+fi
+
+BASHRC="$PERLBREW_ROOT/etc/bashrc"
+
+if [ ! -f $BASHRC ]; then
+    error "no such file $PERLBREW_ROOT/etc/bashrc"
+fi
+
+source $BASHRC
+
+exec perlbrew exec "$@"
