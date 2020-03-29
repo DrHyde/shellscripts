3ff175175c1a499458dd5e78c0dd826cf5f4cbad
David Cantrell <david@cantrell.org.uk>
2020-03-29 10:17:03 +0100

    add script for fetching operavision.eu content

---
+++ get-operavision.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+renice -n 19 -p $$
+
+OPERA_TEMPDIR="get-opera-$$"
+
+mkdir "$OPERA_TEMPDIR"
+(
+    cd "$OPERA_TEMPDIR"
+    youtube-dl -f 136+140 --sub-lang en-GB --write-sub "$@"
+    MP4="$(ls *.mp4)"
+    VTT="$(ls *.vtt)"
+    mv "$MP4" input.mp4
+    ffmpeg -i input.mp4 -vf subtitles="$VTT" "$MP4"
+    mv "$MP4" ..
+)
+rm -rf "$OPERA_TEMPDIR"
