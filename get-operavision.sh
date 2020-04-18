8828fbe7588e0e0c80d3430c2ef7decbb9f3b590
David Cantrell <david@cantrell.org.uk>
2020-04-18 21:38:34 +0100

    cope with video IDs beginning with a hyphen

--- get-operavision.sh
+++ get-operavision.sh
@@ -24,11 +24,11 @@ unlock_queue() {
     rm "$queue_file"
 }
 
-SUBSLANG="$(youtube-dl --list-subs "$1"|grep ^en|tail -1|cut -d ' ' -f1)"
+SUBSLANG="$(youtube-dl --list-subs -- "$1"|grep ^en|tail -1|cut -d ' ' -f1)"
 
 # here beginneth the fetch job
 time -p wait_in_queue fetch
-youtube-dl -f 136+140 --sub-lang $SUBSLANG --write-sub "$1"
+youtube-dl -f 136+140 --sub-lang $SUBSLANG --write-sub -- "$1"
 unlock_queue fetch
 
 MP4="$(ls ./*$1*.mp4)"
